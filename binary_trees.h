d05a69c39c70ce291a39d8b1489bea8b386227a0
SultanJafari <Sultan06668@gmail.com>
2026-08-20 16:47:11 +0000

    Fix binary_trees.h by removing static depth declarations

--- binary_trees.h
+++ binary_trees.h
@@ -48,5 +48,7 @@ static size_t depth(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 static size_t depth(...);
+static size_t depth(const binary_tree_t *tree);
+static size_t depth(...);
 
 #endif
